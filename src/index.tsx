7a4fc0cdcdcf468da2d22e8316186e5a9175465e
joshihritika <123955996+joshihritika@users.noreply.github.com>
2025-05-28 00:02:38 +0200

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,14 @@
-export * from './HomePage';
-export * from './ProjectsPage';
-export * from './NotFoundPage';
-export * from './LamjungMapPage'; 
+import './index.css';
+import React from "react";
+import ReactDOM from 'react-dom/client';
+import { App } from "./App";
+import 'leaflet/dist/leaflet.css';
+
+const rootElement = document.getElementById('root');
+if (rootElement) {
+  ReactDOM.createRoot(rootElement).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+}
