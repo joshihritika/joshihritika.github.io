a842c3d930d8f7a93a60ad5a530725b77fb286a1
joshihritika <123955996+joshihritika@users.noreply.github.com>
2025-05-28 00:03:34 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { HomePage, ProjectsPage, NotFoundPage } from './pages';
+import { HomePage, ProjectsPage, NotFoundPage, LamjungMapPage } from './pages';
 
 export function App() {
   return (
@@ -8,6 +8,7 @@ export function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/projects" element={<ProjectsPage />} />
+        <Route path="/lamjung" element={<LamjungMapPage />} />
         <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </Router>
